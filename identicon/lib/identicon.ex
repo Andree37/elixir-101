3af02480ff7eb236274fbb504de3a99465a27b8d
Andree37 <andresmribeiro@gmail.com>
2022-12-20 19:20:14 +0000

    feat: save image

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -7,6 +7,11 @@ defmodule Identicon do
     |> filter_odd_squares
     |> build_pixel_map
     |> draw_image
+    |> save_image(input)
+  end
+
+  def save_image(image_file, input) do
+    File.write("#{input}.png", image_file)
   end
 
   def draw_image(%Identicon.Image{pixel_map: pixel_map, color: color}) do
