a4a45c5bc64de67b92a1367bdea0696f26ec1ee6
Andree37 <andresmribeiro@gmail.com>
2022-12-20 19:15:32 +0000

    feat: create image

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -6,11 +6,23 @@ defmodule Identicon do
     |> build_grid
     |> filter_odd_squares
     |> build_pixel_map
+    |> draw_image
+  end
+
+  def draw_image(%Identicon.Image{pixel_map: pixel_map, color: color}) do
+    image = :egd.create(250, 250)
+    fill = :egd.color(color)
+
+    Enum.each(pixel_map, fn {start, stop} ->
+      :egd.filledRectangle(image, start, stop, fill)
+    end)
+
+    :egd.render(image)
   end
 
   def build_pixel_map(%Identicon.Image{grid: grid} = image) do
     pixel_map =
-      Enum.map(grid, fn {_, index} = _ ->
+      Enum.map(grid, fn {_, index} ->
         horizontal = rem(index, 5) * 50
         vertical = div(index, 5) * 50
 
